3389d4f7212353fa2d32f7e53abe9e218f524ed4
YunFeng-Zheng <yunfeng0628@gmail.com>
2023-12-25 16:32:07 +0100

    primitivetypes done

--- exercises/04_primitive_types/primitive_types3.rs
+++ exercises/04_primitive_types/primitive_types3.rs
@@ -5,10 +5,9 @@
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
 
 fn main() {
-    let a = ???
+    let a = ["Are we there yet"; 100];
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
@@ -16,4 +15,7 @@ fn main() {
         println!("Meh, I eat arrays like that for breakfast.");
         panic!("Array not big enough, more elements needed")
     }
+
+    print!("{}", a[4].to_owned() +  a[5])
+
 }
