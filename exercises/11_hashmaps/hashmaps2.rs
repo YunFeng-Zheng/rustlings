2e9135fb26891a093d275a34aed6c462e857d521
YunFeng-Zheng <yunfeng0628@gmail.com>
2023-12-27 23:45:20 +0100

    till quiz2

--- exercises/11_hashmaps/hashmaps2.rs
+++ exercises/11_hashmaps/hashmaps2.rs
@@ -14,9 +14,8 @@
 // Execute `rustlings hint hashmaps2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
-use std::collections::HashMap;
+use std::collections::{HashMap, hash_map::Entry};
 
 #[derive(Hash, PartialEq, Eq)]
 enum Fruit {
@@ -28,18 +27,20 @@ enum Fruit {
 }
 
 fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
+
     let fruit_kinds = vec![
         Fruit::Apple,
         Fruit::Banana,
         Fruit::Mango,
         Fruit::Lychee,
-        Fruit::Pineapple,
+        Fruit::Pineapple,   
     ];
 
     for fruit in fruit_kinds {
         // TODO: Insert new fruits if they are not already present in the
         // basket. Note that you are not allowed to put any type of fruit that's
         // already present!
+        basket.entry(fruit).or_insert(5);
     }
 }
 
